a6224ed00f173fd779d7a1e5f62285d19b33a5f7
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-09-24 07:19:29 -0300

    Refactor the server callback function to display a different message based on the route that is taken - using an if/else statement to do so

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const http = require('http') // assign 'http' package to a variable using require
 const server = http.createServer((req, res) => { // Creating and start a server
-  console.log(req.url)
-  res.end('Hello Node.js')
+  if(req.url === '/about')
+    res.end('The about page')
+  else if(req.url ==='/contact')
+    res.end('The contact page')
+  else if(req.url === '/')
+    res.end('The Home page')
+  else {
+    res.writeHead(404)
+    res.end('page not found')
+  }
 })
 
 server.listen(3000)
