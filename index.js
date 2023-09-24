f753353389849f57266f2e6b8b08b80bb074df24
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-09-24 07:11:54 -0300

    Create a basic server to demonstrate request/response cycle

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
+const http = require('http') // assign 'http' package to a variable using require
+const server = http.createServer((req, res) => { // Creating and start a server
   console.log(req.url)
   res.end('Hello Node.js')
 })
+
+server.listen(3000)
