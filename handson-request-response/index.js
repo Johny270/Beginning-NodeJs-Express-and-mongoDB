abf123cb52570f0f3a0d9d74de9fa1ee0218c60d
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-09-24 07:32:27 -0300

    Refactor code so that it respond to requests with html instead of static js text

--- handson-request-response/index.js
+++ handson-request-response/index.js
@@ -1,4 +1,10 @@
 const http = require('http') // assign 'http' package to a variable using require
+const fs=require('fs') // File system module helps interact with file on the server
+const homePage = fs.readFileSync('index.html')
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+const notFoundPage = fs.readFileSync('notfound.html')
+
 const server = http.createServer((req, res) => { // Creating and start a server
 
   /**
@@ -8,14 +14,14 @@ const server = http.createServer((req, res) => { // Creating and start a server
    */
 
   if(req.url === '/about')
-    res.end('The about page')
+    res.end(aboutPage)
   else if(req.url ==='/contact')
-    res.end('The contact page')
+    res.end(contactPage)
   else if(req.url === '/')
-    res.end('The Home page')
+    res.end(homePage)
   else {
     res.writeHead(404)
-    res.end('page not found')
+    res.end(notFoundPage)
   }
 })
 
