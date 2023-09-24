31d595dd1d73856cf49283f854b25ba89ec14943
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-09-24 07:23:46 -0300

    Move index.js and index.html to 'handson-request-response folder

--- handson-request-response/index.js
+++ handson-request-response/index.js
@@ -1,5 +1,12 @@
 const http = require('http') // assign 'http' package to a variable using require
 const server = http.createServer((req, res) => { // Creating and start a server
+
+  /**
+   * This demonstrates a way of handling different
+   * requests from a client and sending the appropriate
+   * response from the server.
+   */
+
   if(req.url === '/about')
     res.end('The about page')
   else if(req.url ==='/contact')
